4fa932f2c0beec3b50fae8b1aa57e7d4450cd22d
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-03-27 13:06:40 +0100

    rename "shows" function to "ramune-api"

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,4 +16,4 @@ app.get("/", returnAllShows);
 app.get("/:showId", returnRequestedShow);
 app.get("/:showId/episodes/:episodeId/stream", returnRequestedShow);
 
-exports.shows = functions.region("europe-west1").https.onRequest(app);
+exports["ramune-api"] = functions.region("europe-west1").https.onRequest(app);
