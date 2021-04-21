4339c4a312db06682c893cddd2f797923c90ac6b
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-21 15:13:27 +0200

    don't ignore typings file (whoops lol)

---
+++ functions/src/typings/types.ts
@@ -0,0 +1,32 @@
+export interface StoredShow {
+	title: string;
+	description: string;
+	seasons: StoredSeason[];
+}
+
+export interface StoredSeason {
+	title: string;
+	episodes: StoredEpisode[];
+}
+
+export interface StoredEpisode {
+	title: string;
+}
+
+export interface Show {
+	id: string;
+	title: string;
+	description: string;
+	poster_url: string;
+	seasons: Season[];
+}
+
+export interface Season {
+	title: string;
+	episodes: Episode[];
+}
+
+export interface Episode {
+	title: string;
+	thumbnail_url: string;
+}
