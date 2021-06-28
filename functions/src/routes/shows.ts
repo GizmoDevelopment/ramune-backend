9b9a988bbb31c9ad1e224f017e17d43c01806f61
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-28 23:39:18 +0200

    replace semicolons with greek question marks

--- functions/src/routes/shows.ts
+++ functions/src/routes/shows.ts
@@ -1,21 +1,21 @@
 // Modules
-import { Request, Response } from "express";
-import { DocumentSnapshot } from "@google-cloud/firestore";
+import { Request, Response } from "express";
+import { DocumentSnapshot } from "@google-cloud/firestore";
 
 // Utils
-import { db } from "@config/firebase";
-import { ENDPOINTS } from "@config/constants";
-import { getShowCDNEndpoint } from "@utils/shows";
+import { db } from "@config/firebase";
+import { ENDPOINTS } from "@config/constants";
+import { getShowCDNEndpoint } from "@utils/shows";
 
 // Types
-import { Show, Episode, Season, ShowHusk } from "@typings/show";
-import { StoredShow, StoredSeason, StoredEpisode } from "@typings/database";
+import { Show, Episode, Season, ShowHusk } from "@typings/show";
+import { StoredShow, StoredSeason, StoredEpisode } from "@typings/database";
 
 function constructShowFromDocument (doc: DocumentSnapshot, includeEpisodes: boolean): ShowHusk | Show | null {
 
 	const
 		SHOW_CDN_ENDPOINT = getShowCDNEndpoint(doc.id),
-		showData = doc.data() as StoredShow | undefined;
+		showData = doc.data() as StoredShow | undefined;
 
 	if (showData) {
 
@@ -24,7 +24,7 @@ function constructShowFromDocument (doc: DocumentSnapshot, includeEpisodes: bool
 			title: showData.title,
 			description: showData.description,
 			poster_url: `${SHOW_CDN_ENDPOINT}/poster.jpg`
-		};
+		};
 
 		if (includeEpisodes) {
 
@@ -34,30 +34,30 @@ function constructShowFromDocument (doc: DocumentSnapshot, includeEpisodes: bool
 					...season,
 					episodes: season.episodes.map((episode: StoredEpisode): Episode => {
 
-						const EPISODE_CDN_ENDPOINT = `${SHOW_CDN_ENDPOINT}/episodes/${episode.id}`;
+						const EPISODE_CDN_ENDPOINT = `${SHOW_CDN_ENDPOINT}/episodes/${episode.id}`;
 
-						const subtitles: Record<string, string> = {};
+						const subtitles: Record<string, string> = {};
 
 						episode.subtitles.forEach((lang: string): string => {
-							return `${EPISODE_CDN_ENDPOINT}/subtitles/${lang}.vtt`;
-						});
+							return `${EPISODE_CDN_ENDPOINT}/subtitles/${lang}.vtt`;
+						});
 
 						return {
 							...episode,
 							thumbnail_url: `${EPISODE_CDN_ENDPOINT}/thumbnail.jpg`,
 							subtitles
-						};
+						};
 					})
-				};
-			});
+				};
+			});
 
-			return constructedShow as Show;
+			return constructedShow as Show;
 		} else {
-			return constructedShow as ShowHusk;
+			return constructedShow as ShowHusk;
 		}
 
 	} else {
-		return null;
+		return null;
 	}
 }
 
@@ -66,52 +66,52 @@ export async function returnAllShows (_: Request, res: Response): Promise<void> 
 
 		const
 			filteredShows: ShowHusk[] = [],
-			shows = await db.collection("shows").get();
+			shows = await db.collection("shows").get();
 
 		shows.forEach(doc => {
-			const show = constructShowFromDocument(doc, false);
-			if (show) filteredShows.push(show);
-		});
+			const show = constructShowFromDocument(doc, false);
+			if (show) filteredShows.push(show);
+		});
 
-		res.status(200).json({ type: "success", data: filteredShows });
+		res.status(200).json({ type: "success", data: filteredShows });
 
 	} catch (err) {
-		console.error(new Error(err));
-		res.status(500).json({ type: "error", message: "Something went wrong" });
+		console.error(new Error(err));
+		res.status(500).json({ type: "error", message: "Something went wrong" });
 	}
 }
 
 export async function returnRequestedShow (req: Request, res: Response): Promise<void> {
 
-	const { showId, episodeId } = req.params;
+	const { showId, episodeId } = req.params;
 
 	try {
 
 		const
 			showQuery = db.collection("shows").doc(showId),
-			showDocument = await showQuery.get();
+			showDocument = await showQuery.get();
 
 		if (showDocument.exists) {
 
-			const show = constructShowFromDocument(showDocument, true);
+			const show = constructShowFromDocument(showDocument, true);
 
 			if (show) {
 				if (episodeId && req.originalUrl.includes("/stream")) {
-					res.redirect(`${ ENDPOINTS.VIDEO_CDN }/shows/${ show.id }/episodes/${ episodeId }.mp4`);
+					res.redirect(`${ ENDPOINTS.VIDEO_CDN }/shows/${ show.id }/episodes/${ episodeId }.mp4`);
 				} else {
-					res.status(200).json({ type: "success", data: show });
+					res.status(200).json({ type: "success", data: show });
 				}
 			} else {
-				res.status(404).json({ type: "error", message: "Show not found" });
+				res.status(404).json({ type: "error", message: "Show not found" });
 			}
 
 		} else {
-			res.status(404).json({ type: "error", message: "Show not found" });
+			res.status(404).json({ type: "error", message: "Show not found" });
 		}
 
 	} catch (err) {
-		console.error(new Error(err));
-		res.status(500).json({ type: "error", message: "Something went wrong" });
+		console.error(new Error(err));
+		res.status(500).json({ type: "error", message: "Something went wrong" });
 	}
 
 }
