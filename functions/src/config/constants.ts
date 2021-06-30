e03ba68f32afe8fa8c8a419823fdfd4ce2c95dd9
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-06-30 13:31:49 +0200

    language code constant

--- functions/src/config/constants.ts
+++ functions/src/config/constants.ts
@@ -1,7 +1,15 @@
 // Modules
 import * as functions from "firebase-functions";
 
+// Types
+import { LanguageCode, LanguageName } from "@typings/subtitles";
+
 export const ENDPOINTS = {
 	CDN: functions.config().cdn.endpoint,
 	VIDEO_CDN: functions.config().video_cdn.endpoint
+};
+
+export const LANGUAGES: Record<LanguageCode, LanguageName> = {
+	"en": "English",
+	"jp": "日本語"
 };
